059f668bc76d89d6dd921dc95f6ae400b590fe4b
Olasunkanmi Idris <olasunkanmiidris15@gmail.com>
2024-09-10 08:44:19 +0100

    fix::(pwa config):: added a skip waiting to ensure the service worker activates immediately

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
+  skipWaiting: true,
   reloadOnOnline: true,
   cacheOnFrontEndNav: true,
   scope: "/",
