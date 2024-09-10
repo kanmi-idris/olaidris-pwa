cf1ee0eff18caf75cb03a3d8a5923de3e32cc00d
Olasunkanmi Idris <olasunkanmiidris15@gmail.com>
2024-09-10 06:26:30 +0100

    updated config with runtime caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,94 @@
 /** @type {import('next').NextConfig} */
 import nextPWA from "next-pwa";
 
+console.log("loading the next config");
+
 const withPWA = nextPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  fallbacks: {
+    document: "/offline",
+  },
+  scope: "/",
+  runtimeCaching: [],
+  reloadOnOnline: true,
   cacheOnFrontEndNav: true,
+  // customWorkerDir: "worker", // This tells next-pwa where to find the custom worker
+  buildExcludes: [
+    // Exclude server-side files from precaching
+    ({ asset }) =>
+      asset.name.includes("server") || asset.name.includes("middleware"),
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-font-assets",
+      },
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-image-assets",
+      },
+    },
+    {
+      urlPattern: /\.(?:js)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-js-assets",
+      },
+    },
+    {
+      urlPattern: /\.(?:css|less)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-style-assets",
+      },
+    },
+    {
+      urlPattern: /\.(?:json|xml|csv)$/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "static-data-assets",
+      },
+    },
+    {
+      urlPattern: /\/api\/.*$/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "apis",
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60, // 1 hour
+        },
+      },
+    },
+    {
+      urlPattern: /.*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "others",
+        networkTimeoutSeconds: 10,
+      },
+    },
+  ],
 });
 
 const nextConfig = {
