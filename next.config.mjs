f4dcc48386523ca66d65c981cae3825cfcb862cb
Olasunkanmi Idris <olasunkanmiidris15@gmail.com>
2024-09-11 11:33:31 +0100

    added runtime caching to fix non immediate caching of next js images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
   reloadOnOnline: true,
   cacheOnFrontEndNav: true,
   scope: "/",
@@ -12,6 +11,33 @@ const withPWA = withPWAInit({
   fallbacks: {
     // Failed page requests fallback to this.
     document: "/~offline",
+  },
+  extendDefaultRuntimeCaching: true,
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: /\/_next\/image\?url=.*\.(png|jpg|jpeg|gif|svg|ico|webp)$/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "next-image-assets",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+          },
+        },
+      },
+      {
+        urlPattern: /\/_next\/image\?url=.+$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-image-other",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+    ],
   },
 });
 
