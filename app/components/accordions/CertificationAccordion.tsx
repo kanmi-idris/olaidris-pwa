8238d59264362efc32a3edccc5a22be1c3fda1fc
Olasunkanmi Idris <olasunkanmiidris15@gmail.com>
2024-09-24 22:02:15 +0100

    feat::(certifications):: completed certifications

---
+++ app/components/accordions/CertificationAccordion.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { DataItem, ICertificate } from "@/types";
+import Image from "next/image";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import "../../globals.css";
+import { formatDate } from "../../utils/helpers";
+
+const CertificationItem = ({
+  data,
+  isOpen,
+  setIsOpen,
+}: {
+  data: ICertificate;
+  setIsOpen: Dispatch<SetStateAction<string | undefined>>;
+  isOpen: string | undefined;
+}) => {
+  const { _id, sponsor, title, awarding_date, logo_uri, proof_uri } = data;
+
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState<number | undefined>(0);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isMobile) {
+      if (isOpen === _id) {
+        setHeight(contentRef.current?.scrollHeight);
+      } else {
+        setHeight(0);
+      }
+    } else {
+      setHeight(contentRef.current?.scrollHeight);
+    }
+  }, [isOpen, _id, isMobile]);
+
+  const handleClick = () => {
+    if (isMobile) {
+      setIsOpen(isOpen === _id ? undefined : _id);
+    }
+  };
+
+  return (
+    <div className="mb-2">
+      <button
+        type="button"
+        className={`w-full py-2 cursor-pointer ${
+          isOpen === _id ? "mb-4" : ""
+        } lg:w-[350px] `}
+        onClick={handleClick}
+      >
+        <div className="flex items-center justify-between">
+          <span className="font-semibold text-offWhite text-[0.83rem] md:text-[1rem] text-start">
+            {sponsor}
+          </span>
+          <Image
+            alt={`${sponsor} logo`}
+            src={logo_uri ?? ""}
+            width={20}
+            height={20}
+            className="inline-block ml-2 rounded"
+          />
+        </div>
+        <div className="flex items-center justify-between mt-1">
+          <span className="text-Black_8 font-medium text-[0.69rem] md:text-[0.83rem] text-start">
+            {title}
+          </span>
+          <span className="text-Black_8 font-medium text-[0.69rem] md:text-[0.83rem] text-end">
+            {`${formatDate(awarding_date)}`}
+          </span>
+        </div>
+      </button>
+
+      <div
+        ref={contentRef}
+        style={{ height: height }}
+        className={`${
+          isMobile
+            ? "overflow-hidden transition-all duration-300 ease-in-out"
+            : ""
+        } w-full lg:w-[350px] ${isOpen !== _id ? "" : "mb-4"}`}
+      >
+        <Image
+          alt={`${sponsor} ${title} certificate`}
+          src={proof_uri ?? ""}
+          width={350}
+          height={350}
+          className={`rounded-lg w-full ${!isMobile && "mt-2"}`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const CertificationsAccordion = ({ items }: { items: DataItem[] }) => {
+  const CertificationItems = items.filter(
+    (item): item is ICertificate => "awarding_date" in item
+  );
+
+  const [isOpen, setIsOpen] = useState<string | undefined>(undefined);
+
+  return (
+    <div className="lg:flex lg:gap-x-8 mt-6 px-4 lg:px-0 overflow-x-scroll items-stretch pb-8 CertificationsAccordion">
+      {CertificationItems.map((item, index) => (
+        <CertificationItem
+          key={index}
+          data={item}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      ))}
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getData } from "./api/controllers";
+import { CertificationsAccordion } from "./components/accordions/CertificationAccordion";
 import { ExperiencesAccordion } from "./components/accordions/ExperienceAccordion";
 import ResponsiveHero from "./components/Hero";
 import SectionTitle from "./components/SectionTitle";
@@ -6,6 +7,7 @@ import Skills from "./components/Skills";
 
 export default async function Home() {
   const experiences = await getData("experiences");
+  const certifications = await getData("certification");
 
   return (
     <main className="pb-16">
@@ -16,9 +18,19 @@ export default async function Home() {
         <ExperiencesAccordion items={experiences} />
       </section>
 
-      <section className="max-w-6xl mx-auto mb-32">
+      <section className="max-w-6xl mx-auto mb-10 lg:mb-24">
         <SectionTitle variant="underlined" title="My Skills" />
         <Skills />
+      </section>
+
+      <section className="max-w-6xl mx-auto mb-10 lg:mb-24">
+        <SectionTitle variant="underlined" title="Certifications" />
+        <CertificationsAccordion items={certifications} />
+      </section>
+
+      <section className="max-w-6xl mx-auto mb-10 lg:mb-24">
+        <SectionTitle variant="underlined" title="Accolades" />
+        <CertificationsAccordion items={certifications} />
       </section>
     </main>
   );
